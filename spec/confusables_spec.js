8a916b2c964c197a949eefcc31528b90a5c1cc4f
Chris Weber <chris@lookout.net>
2015-09-28 21:07:16 -0700

    fixing gulp and jasmine tests

--- spec/confusables_spec.js
+++ spec/confusables_spec.js
@@ -1,19 +1,19 @@
 describe("Test suite for confusables.js getConfusableString() : ", function() {
   it("returns something that does not equal the input", function() {
     var input = "abcDEF";
-    var output = confusables.utility.getConfusableString(input);
+    var output = confusables.getConfusableString(input);
     expect(input).not.toEqual(output);
   });
 
   it("returns a string of equal length", function() {
     var input = "abc123";
-    var output = confusables.utility.getConfusableString(input);
+    var output = confusables.getConfusableString(input);
     expect(output.length).toEqual(6);
   });
 
   it("returns a confusable letter 'A' from the expected set ", function() {
     var input = "A"; // decimal 65
-    var output = confusables.utility.getConfusableString(input);
+    var output = confusables.getConfusableString(input);
     //var expected = ["Α","А","Ꭺ","ᗅ","Ａ","A"];
     var expected = [0xFF21, 0x0391, 0x0410, 0x13AA, 0x15C5 ];
     var success = false;
@@ -28,13 +28,13 @@ describe("Test suite for confusables.js getConfusableString() : ", function() {
     // Thanks to https://ajsblackbelt.wordpress.com/2014/05/18/jasmine-tests-expect-tothrow/comment-page-1/
     // for letting me know to use expect with an anonymous function for this case
     expect(function() {
-      confusables.utility.getConfusableString(1);
+      confusables.getConfusableString(1);
     }).toThrowError();
     expect(function() {
-      confusables.utility.getConfusableString(new Object);
+      confusables.getConfusableString(new Object);
     }).toThrowError();
     expect(function() {
-      confusables.utility.getConfusableString("A");
+      confusables.getConfusableString("A");
     }).not.toThrowError();
   });
 
@@ -43,7 +43,7 @@ describe("Test suite for confusables.js getConfusableString() : ", function() {
     // Get a randon code point outside of the BMP
     var c = Math.floor(Math.random() * 0x10FFFF) + 0xFFFF
     var input = String.fromCodePoint(c); 
-    var output = confusables.utility.getConfusableString(input);
+    var output = confusables.getConfusableString(input);
     expect(input).toEqual(output);
   });
 
@@ -52,7 +52,7 @@ describe("Test suite for confusables.js getConfusableString() : ", function() {
 describe("Test suite for confusables.js getConfusableCharacters() : ", function() {
   it("returns an array", function() {
     var input = "A";
-    var output = confusables.utility.getConfusableCharacters(input);
+    var output = confusables.getConfusableCharacters(input);
     var success = false;
     if (typeof output === "object") {
       success = true;
@@ -68,7 +68,7 @@ describe("Test suite for confusables.js getConfusableCharacters() : ", function(
       // U+29F6 returns an array with length 2, the first 
       // element being an another array of length 2
       // characters[266] = [[0x002F, 0x0304],0x29F6 ];
-      var output = confusables.utility.getConfusableCharacters(String.fromCharCode(input));
+      var output = confusables.getConfusableCharacters(String.fromCharCode(input));
       var success = false;
       if (typeof output === "object" && output.length === 2 && output[0].length === 2) {
           success = true;
@@ -79,7 +79,7 @@ describe("Test suite for confusables.js getConfusableCharacters() : ", function(
   // it("returns a set of expected confusables", function() {
   //     var input = "A";  
   //     // output should be ['A', 'Ａ', 'Α', 'А', 'Ꭺ', 'ᗅ']
-  //     var output = confusables.utility.getConfusableCharacters(input);
+  //     var output = confusables.getConfusableCharacters(input);
   //     expect(output).toEqual(['A', 'Ａ', 'Α', 'А', 'Ꭺ', 'ᗅ']);
   // });
 
